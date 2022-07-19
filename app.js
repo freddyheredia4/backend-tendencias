fbbabb74a9af991f332f650124a5bb00a195ac8e
freddyheredia4 <freddyheredia4@gmail.com>
2022-07-18 20:28:31 -0500

    remove powered by

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const app = express()
+const app1 = express()
+app1.disable("x-powered-by");
 const port = 3000
 
-app.get('/', (req, res) => {
+app1.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
